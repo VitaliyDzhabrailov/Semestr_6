3c2741f8bd93eeba323524278f073b063fbe454b
VitaliyDzhabrailov <vitalyadd22000022@gmail.com>
2023-02-26 17:07:54 +0300

    Всё работает, но проблема в отображении округления. Выводит 9.0, а в контесте обязательно надо 9.000 Сейчас сделаю костыль для этого. Иначе я не нашёл, как такое чинить

--- Lesson 2/Homework/Task 3.py
+++ Lesson 2/Homework/Task 3.py
@@ -27,5 +27,26 @@
 """
 
 import pandas as pd
-import numpy as np
 
+df_games_path = str(input())
+df_rates_path = str(input())
+
+df_games = pd.read_csv(df_games_path, sep=";")
+df_rates = pd.read_csv(df_rates_path, sep=";")
+
+df_rates_mean = df_rates.groupby(['id'], as_index=False).mean()
+df_rates_mean.sort_values(by='mark', ascending=False, inplace=True, ignore_index=True)
+print(df_games[df_games['id'] == df_rates_mean['id'][0]]['name'].values[0], df_rates_mean['mark'][0].round(3))
+print(df_games[df_games['id'] == df_rates_mean['id'][1]]['name'].values[0], df_rates_mean['mark'][1].round(3))
+print(df_games[df_games['id'] == df_rates_mean['id'][2]]['name'].values[0], df_rates_mean['mark'][2].round(3))
+
+df_high_rates_index = df_rates_mean[df_rates_mean['mark'] > 8]
+df_high_rates_games = df_games[df_games.id.isin(df_high_rates_index.id) == True].assign(score=lambda x: 1)
+df_high_rates_games = df_high_rates_games.groupby('company', as_index=False).sum(numeric_only=True)
+df_high_rates_games.sort_values(by='score', ascending=False, inplace=True, ignore_index=True)
+print(df_high_rates_games.company[0], df_high_rates_games.score[0])
+
+"""
+games001.csv
+rates001.csv
+"""
