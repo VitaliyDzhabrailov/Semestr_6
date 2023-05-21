14bda2c56ba8e166e6e1b262a6348a08262e8cf4
VitaliyDzhabrailov <vitalyadd22000022@gmail.com>
2023-05-21 15:29:29 +0300

    html task 2 Вообще не понимаю, что в чём проблема. Сохранение темы работает, но почему-то она при перезагрузке и первом запуске не накидывает ccs с темой самой. Хотя вроде всё логично. Может это из-за того, как работает flask. У меня в первом задании с ним странности были, когда я ссылку делал. Ему ссылки надо как-то по-другому скармливать. А тут вообще на php часть кода... Может поэтому он ругается

---
+++ Lesson 7/Homework/static/theme.php
@@ -0,0 +1,7 @@
+session_start();
+
+if(!isset($_SESSION["theme"]))
+{
+    $_SESSION["theme"] = "dark";
+}
+
---
+++ Lesson 7/Homework/static/themes.php
@@ -0,0 +1,10 @@
+if(isset($_GET["theme"]))
+{
+    $theme = $_GET["theme"];
+
+    if($theme == "light" || $theme == "dark")
+    {
+   	 $_SESSION["theme"] = $theme;
+    }
+}
+
